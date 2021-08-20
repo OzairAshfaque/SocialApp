527c1a3498dac519e79c57b2a69d8941c89d5527
Ozair <ozair.ashfaque@gmail.com>
2021-08-20 13:18:17 +1000

    AppUser New Properties added in entity along with Photo class

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -6,6 +6,7 @@ public class RegiserDto
 {
     [Required]
     public string Username { get; set; }
+    
     [Required]
     [StringLength(maximumLength:10,MinimumLength =4)]
     public string Password { get; set; }
