818f34ffd42603403b252e5f7bedd1141c686685
Ozair <ozair.ashfaque@gmail.com>
2021-08-31 13:11:28 +1000

    Adding AppUser-MemberDTo, Photo-PhotoDto, AutoMapper Initialization

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using API.Controllers;
+using API.Interfaces;
 
 namespace API.Controllers
 {
@@ -17,9 +18,11 @@ namespace API.Controllers
     {
 
         private readonly DataContext _context;
-        public UsersController(DataContext context)
+        private readonly IUserRepository _repository;
+        public UsersController(DataContext context, IUserRepository repository)
         {
             _context = context;
+            _repository = repository;
         }
 
         [AllowAnonymous]
@@ -30,14 +33,26 @@ namespace API.Controllers
            // var usr = await _context.Users.ToListAsync();
            
             //return Ok(usr.Select(x=>x.UserName).Where(x=>x=="p"));//
-            return await _context.Users.ToListAsync();
+           var users = await _repository.GetUsersAsync();
+            return Ok(users);//await _context.Users.ToListAsync();
         }
         
-        [HttpGet("{id}")]
+     /*   [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUsersById(int id)
         {
           // return _context.Users.FindAsync(id);
-            return await _context.Users.FirstOrDefaultAsync(x=>x.Id==id);
+          // await _context.Users.Include(p=>p.Photos).Where(x=>x.Id==id).FirstOrDefaultAsync(x=>x.Id==id);
+          return await _repository.GetUserByIdAsync(id);
+        }
+        */
+
+                
+        [HttpGet("{username}")]
+        public async Task<ActionResult<AppUser>> GetUsersByUsername(string username)
+        {
+          // return _context.Users.FindAsync(id);
+        // await _context.Users.Include(p=>p.Photos).Where(x=>x.Id==id).FirstOrDefaultAsync(x=>x.Id==id);
+            return await _repository.GetUserByUsernameAsync(username);
         }
 
         [HttpDelete("{id}")]
--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using API.Helpers;
 using API.Interfaces;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
@@ -12,10 +13,12 @@ namespace API.Extensions
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration _config)
         {
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepository>();
             services.AddDbContext<DataContext>(options=>
             {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             return services;
 
         }
