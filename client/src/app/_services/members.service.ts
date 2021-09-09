f59337ffdb7629e927d43cde3dbef4d17374f480
Ozair <ozair.ashfaque@gmail.com>
2021-09-09 21:25:10 +1000

    member-service state saved in Member array(avoided going back to api), Member-list-component, loadMember Deprecated and Observable Member used to load member, async used to get member lists on template

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 
@@ -9,18 +11,44 @@ import { Member } from '../_models/member';
 })
 export class MembersService {
   BaseUrl = environment.apiUrl;
+  members : Member[] =[];
   
 
   constructor(private http : HttpClient) { }
   getMembers(){
-    return this.http.get<Member[]>(this.BaseUrl + 'users');
+    if(this.members.length > 0)
+    {
+      console.log(this.members +"THis is beofr if");
+      return of(this.members);
+    } 
+    return this.http.get<Member[]>(this.BaseUrl + 'users').pipe(
+      map(members =>{
+        members.forEach(element => {
+          console.log(element.username +"THis is after if");
+        });
+        this.members = members;
+        
+        return this.members;
+      })
+    );
   }
 
   getMember(username :string){
-    return this.http.get<Member>(this.BaseUrl + 'users/' + username)
+    const member = this.members.find(x=>x.username === username);
+    if(member !== undefined) 
+    {
+      console.log('member'+member.username);
+      return of (member);
+    }
+    return this.http.get<Member>(this.BaseUrl + 'users/' + username);
   }
 
   updateMember(member:Member){
-    return this.http.put(this.BaseUrl+'users',member);
+    return this.http.put(this.BaseUrl+'users',member).pipe(
+      map(()=>{
+        const index = this.members.indexOf(member);
+        this.members[index] = member;
+         }
+      ));
   }
 }
